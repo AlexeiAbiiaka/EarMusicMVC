1edc857d8d1fe8c10823d97ab85de8a6daa4fbc4
abiiaka <abiiaka@abiiakas-macbook-pro.local>
2018-03-27 11:18:26 -0500

    initial commit

---
+++ EarMusicMVC/src/earmusicmvc/Client.java
@@ -0,0 +1,57 @@
+package earmusicmvc;
+
+// **********************************************************************
+// Programmer:	Alexei Abiiaka
+// Class:		CS30S
+//
+// Assignment:	aX qy
+//
+// Description:	a brief description of the program goes here
+//
+//
+//	Input:		describe any input from keyboard or file
+//
+//  Output:		describe the result of your program
+// ***********************************************************************
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+public class Client
+ {  // begin class
+ 	
+        private static int nextID = 1000;
+    
+ 	// *********** class constants **********
+        
+        private static final int beatsRate = 300;
+        private static final int boseRate = 150;
+ 	private static final int jblRate = 200;
+        private static final int skullCandyRate = 100;
+        
+        private static final int headphonesHeadset = 20;
+        private static final int earbudsHeadset = 10;
+        
+        private static final int wirelessRate = 50;
+        private static final int noneWirelessRate = 5;
+        
+        private static final int microphoneRate = 20;
+        private static final int noneMicrophoneRate = 5;
+        
+ 	// ********** instance variable **********
+        
+        private String firstName;
+        private String lastName;
+        
+        private char brandType;
+        private char headsetType;
+        private char wirelessType;
+        private char microphoneType;
+ 	
+ 	// ********** constructors ***********
+ 	
+ 	// ********** accessors **********
+ 	
+ 	// ********** mutators **********
+ 
+ }  // end class
---
+++ EarMusicMVC/src/earmusicmvc/EarMusicMVC.java
@@ -0,0 +1,71 @@
+package earmusicmvc;
+
+/********************************************************************
+ * Programmer:	Alexei Abiiaka
+ * Class:  CS30S
+ *
+ * Assignment: Ax Qy
+ * Program Name:  name of public class
+ *
+ * Description: brief description of program
+ *
+ * Input: data to be input
+ *
+ * Output: results to be output
+ ***********************************************************************/
+ 
+ // import java libraries here as needed
+ 
+ import javax.swing.*;
+ import java.text.DecimalFormat;
+
+public class EarMusicMVC {  // begin class
+    
+    public static void main(String[] args) {  // begin main
+    
+    // ********* declaration of constants **********
+    
+    // ********** declaration of variables **********
+    
+    	String strin;				// string data input from keyboard
+    	String strout;				// processed info string to be output
+    	String bannerOut;			// string to print banner to message dialogs
+    	
+    	String prompt;				// prompt for use in input dialogs
+    	
+    	String delim = "[ :]+";		// delimiter string for splitting input string
+    	String tabSpace = "      ";	// six spaces
+    	
+    // create an instance of the ConsoleReader so that we can read data from the keyboard
+    
+    	//ConsoleReader console = new ConsoleReader(System.in);
+    	//DecimalFormat df1 = new DecimalFormat("##.###");
+    	
+    // ********** Print output Banner **********
+    
+    	System.out.println("*******************************************");
+    	System.out.println("Name:		Alexei Abiiaka");
+    	System.out.println("Class:		CS30S");
+    	System.out.println("Assignment:	Ax Qy");
+    	System.out.println("*******************************************");
+
+    	bannerOut = "*******************************************\n";
+    	bannerOut += "Name:		Alexei Abiiaka\n";
+    	bannerOut += "Class:		CS30S\n";
+    	bannerOut += "Assignment:	Ax Qy\n";
+    	bannerOut += "*******************************************\n\n";
+    	
+    // ************************ get input **********************
+    	
+    // ************************ processing ***************************
+	
+
+ 
+    // ************************ print output ****************************
+    
+    
+        // ******** closing message *********
+        
+        System.out.println("end of processing.");
+    }  // end main
+}  // end class
